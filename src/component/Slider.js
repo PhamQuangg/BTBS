be0bae2bfb263a58c1f983f0937e0b28ed31acd2
PhamQuangg <quang125zxc@gmail.com>
2022-05-31 23:40:41 +0700

    all

---
+++ src/component/Slider.js
@@ -0,0 +1,18 @@
+
+export default function Slider() {
+    return (
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="" class="d-block w-100" alt="../img/slider1.png"/>
+    </div>
+    <div class="carousel-item">
+      <img src="" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+</div>
+    )
+}
